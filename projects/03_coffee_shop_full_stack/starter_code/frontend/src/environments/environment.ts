cadc96baa8905559a51e598b0779e05181ab5727
kemaltulum <tulum.ke@gmail.com>
2020-05-25 19:57:24 +0300

    Update environment variables for frontend

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'fsnd-kml', // the auth0 domain prefix
+    audience: 'Trial', // the audience set for the auth0 app
+    clientId: 'OjyTxs32jUewW0U7ds0gHPP2Qonic0u5', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
